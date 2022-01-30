6669e1580aad2bb45d90fbaa7189bc0cb4e76101
Edwin Wood <edwinjwood3rd@gmail.com>
2022-01-30 00:12:01 -0700

    all the lua

---
+++ nvim/lua/options.lua
@@ -0,0 +1,17 @@
+vim.cmd "filetype plugin indent on"
+
+vim.o.backspace='indent,eol,start'
+vim.o.tabstop=2
+vim.o.shiftwidth=2
+vim.o.ruler=true
+vim.o.number=true
+vim.o.showcmd=true
+vim.o.incsearch=true
+vim.o.hlsearch=true
+vim.o.cursorline=true
+vim.o.path=os.getenv("PWD").."/**"
+vim.o.encoding="UTF-8"
+vim.o.completeopt="menu,menuone,noselect"
+vim.o.termguicolors=true
+
+vim.g.mapleader = ','
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -36,4 +36,8 @@ return require'packer'.startup(function()
 	use 'ryanoasis/vim-devicons'
 
 	use 'marko-cerovac/material.nvim'
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+	}
 end)
