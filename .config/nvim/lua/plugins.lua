debc665d6505504cb01081a08f563f668c69db45
Edwin Wood <edwinjwood3rd@gmail.com>
2022-01-29 23:15:58 -0700

    -> lua

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,39 @@
+vim.cmd "packadd packer.nvim"
+
+return require'packer'.startup(function()
+	use 'wbthomason/packer.nvim'
+
+	use 'haya14busa/incsearch.vim'
+	use 'junegunn/fzf'
+	use 'junegunn/fzf.vim'
+	use 'ojroques/nvim-lspfuzzy'
+	
+	use 'simrat39/symbols-outline.nvim'
+	use 'neovim/nvim-lspconfig'
+
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/cmp-path'
+	use 'hrsh7th/cmp-cmdline'
+	use 'hrsh7th/nvim-cmp'
+	use 'hrsh7th/cmp-vsnip'
+	use 'hrsh7th/vim-vsnip'
+	use 'weilbith/nvim-code-action-menu'
+
+	use 'sbdchd/neoformat'
+	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+	use 'nvim-treesitter/playground'
+	use 'jiangmiao/auto-pairs'
+
+	use 'tpope/vim-rails'
+	use 'thoughtbot/vim-rspec'
+	
+	use 'tpope/vim-fugitive'
+	use 'tpope/vim-rhubarb'
+
+	use 'preservim/nerdtree'
+	use 'Xuyuanp/nerdtree-git-plugin'
+	use 'ryanoasis/vim-devicons'
+
+	use 'marko-cerovac/material.nvim'
+end)
