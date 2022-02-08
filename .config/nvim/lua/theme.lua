aa0de74f320fa1608c2c8e81a5e75d91fa7c1693
Trey Wood <treyw@squareup.com>
2022-02-07 19:38:16 -0700

    some updates

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -2,7 +2,10 @@ vim.o.background='dark'
 vim.cmd [[
 colorscheme everforest
 
-hi TSParameter guifg=#a7b0a4
+hi Gray guifg=#a7b0a4
+
+hi! link TSParameter Gray
+hi! link TSOperator Gray
 hi! link TSField TSVariable
 
 hi! link rubyTSSymbol Orange
