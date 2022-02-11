b654838992aeac6c44f07454be9999a88f76db52
Trey Wood <treyw@squareup.com>
2022-02-10 22:06:54 -0700

    formatting

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -2,7 +2,7 @@ Keymaps = {}
 
 function Keymaps.setup_keymaps()
   local opts = { noremap=true, silent=true }
-  vim.api.nvim_set_keymap('n','<C-e>',"<cmd>lua require'telescope.builtin'.oldfiles{cwd_only=true}<CR>", opts)
+  vim.api.nvim_set_keymap('n','<C-e>',':Telescope oldfiles cwd_only=true<CR>', opts)
   vim.api.nvim_set_keymap('n','<C-p>',':Telescope git_files<CR>', opts)
   vim.api.nvim_set_keymap('n','<C-f>',':Telescope live_grep<CR>', opts)
   vim.api.nvim_set_keymap('n','<C-b>',':Telescope buffers<CR>', opts)
@@ -12,19 +12,12 @@ function Keymaps.setup_keymaps()
   vim.cmd "let g:incsearch#auto_nohlsearch=1"
   vim.api.nvim_set_keymap('','/','<Plug>(incsearch-forward)', opts)
   vim.api.nvim_set_keymap('','?','<Plug>(incsearch-backward)', opts)
-  vim.api.nvim_set_keymap('','g/','<Plug>(incsearch-stay)', opts)
   vim.api.nvim_set_keymap('','n','<Plug>(incsearch-nohl-n)', opts)
   vim.api.nvim_set_keymap('','N','<Plug>(incsearch-nohl-N)', opts)
-  vim.api.nvim_set_keymap('','*','<Plug>(incsearch-nohl-*)', opts)
-  vim.api.nvim_set_keymap('','#','<Plug>(incsearch-nohl-#)', opts)
-  vim.api.nvim_set_keymap('','g*','<Plug>(incsearch-nohl-g*)', opts)
-  vim.api.nvim_set_keymap('','g#','<Plug>(incsearch-nohl-g#)', opts)
 
   vim.api.nvim_set_keymap('n','-',':NERDTreeToggle<CR>', opts)
   vim.api.nvim_set_keymap('n','=',':NERDTreeFind<CR>', opts)
 
-  --vim.api.nvim_set_keymap('n','<leader>t',':call RunNearestSpec()<CR>', opts)
-  --vim.api.nvim_set_keymap('n','<leader>r',':call RunLastSpec()<CR>', opts)
   vim.api.nvim_set_keymap('n','<leader>tn',':TestNearest<CR>', opts)
   vim.api.nvim_set_keymap('n','<leader>tf',':TestFile<CR>', opts)
   vim.api.nvim_set_keymap('n','<leader>tt',':TestLast<CR>', opts)
