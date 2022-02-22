d34ffc356a0e9b3d12f573c8b56f46bbedeb7b0f
Trey Wood <treyw@squareup.com>
2022-02-21 20:51:13 -0700

    updates

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -7,6 +7,7 @@ let g:everforest_diagnostic_text_highlight = 1
 colorscheme everforest
 
 hi Gray guifg=#a7b0a4
+hi GreenBold guifg=#a7c080 gui=bold
 
 hi! link TSParameter Gray
 hi! link TSOperator Gray
@@ -18,5 +19,16 @@ hi! link rubyTSLabel TSVariable
 hi! link GitSignsAdd Green
 hi! link GitSignsChange Orange
 hi! link GitSignsDelete Red
+
+hi! link MiniStarterHeader Green
+hi! link MiniStarterSection Red
+hi! link MiniStarterItem Fg
+hi! link MiniStarterItemBullet GreenBold
+hi! link MiniStarterItemPrefix GreenBold
+hi! link MiniStarterQuery Orange
+hi! link MiniStarterFooter Gray
+
+hi IndentBlanklineIndent1 guibg=NONE gui=nocombine
+hi IndentBlanklineIndent2 guibg=#323c41 gui=nocombine
 ]]
 
