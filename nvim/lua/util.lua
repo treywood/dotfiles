acfe541028e9918c3e2595ba71f5fa43e5523691
Trey Wood <treyw@squareup.com>
2022-03-05 21:43:59 -0700

    stylua config

--- nvim/lua/util.lua
+++ nvim/lua/util.lua
@@ -1,14 +1,14 @@
 local M = {}
 
 function M.augroups(config)
-	for group, commands in pairs(config) do
-		vim.cmd("augroup " .. group)
-		vim.cmd("au!")
-		for _, cmd in pairs(commands) do
-			vim.cmd("au " .. cmd)
-		end
-		vim.cmd("augroup END")
-	end
+  for group, commands in pairs(config) do
+    vim.cmd('augroup ' .. group)
+    vim.cmd('au!')
+    for _, cmd in pairs(commands) do
+      vim.cmd('au ' .. cmd)
+    end
+    vim.cmd('augroup END')
+  end
 end
 
 return M
