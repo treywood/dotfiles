5d87b618de8a9b5c79a07d2014a5bff6408a2028
Trey Wood <treyw@squareup.com>
2022-04-15 13:16:30 -0600

    nvim_set_keymap -> vim.keymap.set

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -2,91 +2,80 @@ Keymaps = {}
 
 function Keymaps.setup()
   local opts = { silent = true }
-  vim.api.nvim_set_keymap('n', '<C-e>', ':Telescope oldfiles cwd_only=true<CR>', opts)
-  vim.api.nvim_set_keymap('n', '<C-p>', ':Telescope git_files<CR>', opts)
-  vim.api.nvim_set_keymap('n', '<C-f>', ':Telescope live_grep<CR>', opts)
-  vim.api.nvim_set_keymap('n', '<C-b>', ':Telescope buffers<CR>', opts)
-  vim.api.nvim_set_keymap('n', '<C-j>', ':Telescope lsp_document_symbols<CR>', opts)
+  vim.keymap.set('n', '<C-e>', ':Telescope oldfiles cwd_only=true<CR>', opts)
+  vim.keymap.set('n', '<C-p>', ':Telescope git_files<CR>', opts)
+  vim.keymap.set('n', '<C-f>', ':Telescope live_grep<CR>', opts)
+  vim.keymap.set('n', '<C-b>', ':Telescope buffers<CR>', opts)
+  vim.keymap.set('n', '<C-j>', ':Telescope lsp_document_symbols<CR>', opts)
 
-  vim.api.nvim_set_keymap('', '/', '<Plug>(incsearch-forward)', opts)
-  vim.api.nvim_set_keymap('', '?', '<Plug>(incsearch-backward)', opts)
-  vim.api.nvim_set_keymap('', 'n', '<Plug>(incsearch-nohl-n)', opts)
-  vim.api.nvim_set_keymap('', 'N', '<Plug>(incsearch-nohl-N)', opts)
+  vim.keymap.set('', '/', '<Plug>(incsearch-forward)', opts)
+  vim.keymap.set('', '?', '<Plug>(incsearch-backward)', opts)
+  vim.keymap.set('', 'n', '<Plug>(incsearch-nohl-n)', opts)
+  vim.keymap.set('', 'N', '<Plug>(incsearch-nohl-N)', opts)
 
-  vim.api.nvim_set_keymap('n', '-', ':NvimTreeToggle<CR>', opts)
-  vim.api.nvim_set_keymap('n', '=', ':NvimTreeFindFile<CR>', opts)
+  vim.keymap.set('n', '-', ':NvimTreeToggle<CR>', opts)
+  vim.keymap.set('n', '=', ':NvimTreeFindFile<CR>', opts)
 
-  vim.api.nvim_set_keymap('n', '<leader>tn', ':TestNearest<CR>', opts)
-  vim.api.nvim_set_keymap('n', '<leader>tf', ':TestFile<CR>', opts)
-  vim.api.nvim_set_keymap('n', '<leader>tt', ':TestLast<CR>', opts)
+  vim.keymap.set('n', '<leader>tn', ':TestNearest<CR>', opts)
+  vim.keymap.set('n', '<leader>tf', ':TestFile<CR>', opts)
+  vim.keymap.set('n', '<leader>tt', ':TestLast<CR>', opts)
 
-  vim.api.nvim_set_keymap('n', '<leader>W', ':%bd<CR>', opts)
-  vim.api.nvim_set_keymap('n', '<leader>w', ':%bd|e#|bd#<CR>', opts)
+  vim.keymap.set('n', '<leader>W', ':%bd<CR>', opts)
+  vim.keymap.set('n', '<leader>w', ':%bd|e#|bd#<CR>', opts)
 
-  vim.api.nvim_set_keymap('', '<leader>gy', ':GBrowse!<CR>', opts)
+  vim.keymap.set('', '<leader>gy', ':GBrowse!<CR>', opts)
 
-  vim.api.nvim_set_keymap('n', ']c', '<cmd>Gitsigns next_hunk<CR>', opts)
-  vim.api.nvim_set_keymap('n', '[c', '<cmd>Gitsigns prev_hunk<CR>', opts)
-  vim.api.nvim_set_keymap('n', '<leader>hp', '<cmd>Gitsigns preview_hunk<CR>', opts)
-  vim.api.nvim_set_keymap('n', '<leader>hu', '<cmd>Gitsigns reset_hunk<CR>', opts)
+  vim.keymap.set('n', ']c', '<cmd>Gitsigns next_hunk<CR>', opts)
+  vim.keymap.set('n', '[c', '<cmd>Gitsigns prev_hunk<CR>', opts)
+  vim.keymap.set('n', '<leader>hp', '<cmd>Gitsigns preview_hunk<CR>', opts)
+  vim.keymap.set('n', '<leader>hu', '<cmd>Gitsigns reset_hunk<CR>', opts)
 
-  vim.api.nvim_set_keymap('n', '<leader>cn', ':cnext<CR>', opts)
-  vim.api.nvim_set_keymap('n', '<leader>cN', ':cprevious<CR>', opts)
+  vim.keymap.set('n', '<leader>cn', ':cnext<CR>', opts)
+  vim.keymap.set('n', '<leader>cN', ':cprevious<CR>', opts)
 
-  vim.api.nvim_set_keymap('n', '<M-K>', 'ddkP', opts)
-  vim.api.nvim_set_keymap('n', '<M-J>', 'ddp', opts)
+  vim.keymap.set('n', '<M-K>', 'ddkP', opts)
+  vim.keymap.set('n', '<M-J>', 'ddp', opts)
 
-  vim.api.nvim_set_keymap('n', '<leader>gy', ':GBrowse<CR>', opts)
+  vim.keymap.set('n', '<leader>gy', ':GBrowse<CR>', opts)
 
-  vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', opts)
+  vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', opts)
 
-  vim.api.nvim_set_keymap('i', '<C-j>', "<C-O>:lua require('luasnip').change_choice(1)<CR>", opts)
+  vim.keymap.set('i', '<C-j>', function()
+    require('luasnip').change_choice(1)
+  end, opts)
 
-  vim.api.nvim_set_keymap('i', '<C-Space>i', '<Plug>(sq-connect-insert-id)', opts)
-  vim.api.nvim_set_keymap('i', '<C-Space>l', '<Plug>(sq-connect-insert-location-id)', opts)
-  vim.api.nvim_set_keymap('i', '<C-Space>m', '<Plug>(sq-connect-insert-merchant-id)', opts)
-  vim.api.nvim_set_keymap('i', '<C-Space>c', '<Plug>(sq-connect-insert-customer-id)', opts)
+  vim.keymap.set('i', '<C-Space>i', '<Plug>(sq-connect-insert-id)', opts)
+  vim.keymap.set('i', '<C-Space>l', '<Plug>(sq-connect-insert-location-id)', opts)
+  vim.keymap.set('i', '<C-Space>m', '<Plug>(sq-connect-insert-merchant-id)', opts)
+  vim.keymap.set('i', '<C-Space>c', '<Plug>(sq-connect-insert-customer-id)', opts)
 
-  vim.api.nvim_set_keymap('v', '<C-Space>i', '<Plug>(sq-connect-insert-id)', opts)
-  vim.api.nvim_set_keymap('v', '<C-Space>l', '<Plug>(sq-connect-insert-location-id)', opts)
-  vim.api.nvim_set_keymap('v', '<C-Space>m', '<Plug>(sq-connect-insert-merchant-id)', opts)
-  vim.api.nvim_set_keymap('v', '<C-Space>c', '<Plug>(sq-connect-insert-customer-id)', opts)
+  vim.keymap.set('v', '<C-Space>i', '<Plug>(sq-connect-insert-id)', opts)
+  vim.keymap.set('v', '<C-Space>l', '<Plug>(sq-connect-insert-location-id)', opts)
+  vim.keymap.set('v', '<C-Space>m', '<Plug>(sq-connect-insert-merchant-id)', opts)
+  vim.keymap.set('v', '<C-Space>c', '<Plug>(sq-connect-insert-customer-id)', opts)
 
-  vim.api.nvim_set_keymap('s', '<C-Space>i', '<Plug>(sq-connect-insert-id)', opts)
-  vim.api.nvim_set_keymap('s', '<C-Space>l', '<Plug>(sq-connect-insert-location-id)', opts)
-  vim.api.nvim_set_keymap('s', '<C-Space>m', '<Plug>(sq-connect-insert-merchant-id)', opts)
-  vim.api.nvim_set_keymap('s', '<C-Space>c', '<Plug>(sq-connect-insert-customer-id)', opts)
+  vim.keymap.set('s', '<C-Space>i', '<Plug>(sq-connect-insert-id)', opts)
+  vim.keymap.set('s', '<C-Space>l', '<Plug>(sq-connect-insert-location-id)', opts)
+  vim.keymap.set('s', '<C-Space>m', '<Plug>(sq-connect-insert-merchant-id)', opts)
+  vim.keymap.set('s', '<C-Space>c', '<Plug>(sq-connect-insert-customer-id)', opts)
 
   opts = { noremap = true, silent = true }
-  vim.api.nvim_set_keymap('n', ';', 'm', opts)
-  vim.api.nvim_set_keymap('n', "'", '`', opts)
+  vim.keymap.set('n', ';', 'm', opts)
+  vim.keymap.set('n', "'", '`', opts)
 end
 
 function Keymaps.lsp_keymaps(bufnr)
-  local function buf_set_keymap(...)
-    vim.api.nvim_buf_set_keymap(bufnr, ...)
-  end
-  local function buf_set_option(...)
-    vim.api.nvim_buf_set_option(bufnr, ...)
-  end
-
-  -- Enable completion triggered by <c-x><c-o>
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-  local opts = { noremap = true, silent = true }
-
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', ':Telescope lsp_definitions<CR>', opts)
-  buf_set_keymap('n', 'gi', ':Telescope lsp_implementations<CR>', opts)
-  buf_set_keymap('n', 'gr', ':Telescope lsp_references<CR>', opts)
-  buf_set_keymap('n', '<leader>a', ':Telescope lsp_code_actions theme=cursor<CR>', opts)
-  buf_set_keymap('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-  buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+  local opts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+  vim.keymap.set('n', 'gd', ':Telescope lsp_definitions<CR>', opts)
+  vim.keymap.set('n', 'gi', ':Telescope lsp_implementations<CR>', opts)
+  vim.keymap.set('n', 'gr', ':Telescope lsp_references<CR>', opts)
+  vim.keymap.set('n', '<leader>a', ':Telescope lsp_code_actions theme=cursor<CR>', opts)
+  vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 end
 
 return Keymaps
