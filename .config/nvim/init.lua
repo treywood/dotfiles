c9e75e9271363c05ad21a08ea1c4151adea529eb
Trey Wood <treyw@squareup.com>
2022-05-09 08:52:34 -0600

    custom mini.starter keymaps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,3 +4,14 @@ require('theme')
 require('keymaps').setup()
 require('lsp')
 require('autocommands')
+
+vim.api.nvim_create_user_command('Notes', function(opts)
+  local name = opts.fargs[1] or 'notes'
+  vim.cmd(string.format('tabe ~/notes/%s', name))
+end, {
+  nargs = '?',
+  force = true,
+  complete = function()
+    return vim.fn.readdir(string.format('%s/notes/', os.getenv('HOME')))
+  end,
+})
--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -7,3 +7,21 @@ vim.api.nvim_create_autocmd('FileType', {
     })
   end,
 })
+
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'MiniStarterOpened',
+  callback = function(args)
+    local MiniStarter = require('mini.starter')
+    local options = { buffer = args.buf }
+    local next_item = function()
+      MiniStarter.update_current_item('next')
+    end
+    local prev_item = function()
+      MiniStarter.update_current_item('prev')
+    end
+
+    vim.keymap.set('n', '<C-j>', next_item, options)
+    vim.keymap.set('n', '<C-k>', prev_item, options)
+    vim.keymap.set('n', '<C-p>', ':Telescope git_files<CR>', options)
+  end,
+})
