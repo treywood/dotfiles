da25aeb986291ebdffe84024b881cd40cc85e796
Trey Wood <treyw@squareup.com>
2022-06-08 13:41:18 -0600

    update nvim-tree config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -173,36 +173,6 @@ return require('packer').startup(function()
       'kyazdani42/nvim-web-devicons',
     },
     config = function()
-      vim.g.nvim_tree_show_icons = {
-        git = 1,
-        folders = 1,
-        files = 1,
-        folder_arrows = 1,
-      }
-      vim.g.nvim_tree_icons = {
-        default = '',
-        symlink = '',
-        git = {
-          unstaged = '✗',
-          staged = '✓',
-          unmerged = '',
-          renamed = '➜',
-          untracked = '★',
-          deleted = '',
-          ignored = '◌',
-        },
-        folder = {
-          arrow_open = '',
-          arrow_closed = '',
-          default = '',
-          open = '',
-          empty = '',
-          empty_open = '',
-          symlink = '',
-          symlink_open = '',
-        },
-      }
-
       require('nvim-tree').setup {
         filters = {
           dotfiles = true,
@@ -211,6 +181,39 @@ return require('packer').startup(function()
           mappings = {
             list = {
               { key = '-', action = 'close' },
+            },
+          },
+        },
+        renderer = {
+          icons = {
+            show = {
+              git = true,
+              folder = true,
+              file = true,
+              folder_arrow = true,
+            },
+            glyphs = {
+              default = '',
+              symlink = '',
+              git = {
+                unstaged = '✗',
+                staged = '✓',
+                unmerged = '',
+                renamed = '➜',
+                untracked = '★',
+                deleted = '',
+                ignored = '◌',
+              },
+              folder = {
+                arrow_open = '',
+                arrow_closed = '',
+                default = '',
+                open = '',
+                empty = '',
+                empty_open = '',
+                symlink = '',
+                symlink_open = '',
+              },
             },
           },
         },
