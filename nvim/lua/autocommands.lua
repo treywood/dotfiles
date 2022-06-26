5cb0db45fa864028baaae1108912794cba56bd61
Trey Wood <treyw@squareup.com>
2022-06-26 08:35:00 -0600

    default tab title when argc > 0 is fine

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -50,15 +50,11 @@ end
 
 vim.api.nvim_create_autocmd('VimEnter', {
   callback = function()
-    local tab_name
     if vim.fn.argc() == 0 then
       local repo_name = vim.fn.system([[git remote get-url --all origin | sed -E 's/.+\/(.+)\.git$/\1/']])
-      tab_name = string.format('nvim (%s)', repo_name:gsub('%s*$', ''))
-    else
-      local file_name = vim.fn.argv(0)
-      tab_name = string.format('nvim (%s)', file_name)
+      local tab_name = string.format('nvim (%s)', repo_name:gsub('%s*$', ''))
+      set_tab_title(tab_name)
     end
-    set_tab_title(tab_name)
   end,
 })
 vim.api.nvim_create_autocmd('VimLeave', {
