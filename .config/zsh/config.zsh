234200990cfad8793d309882744fbdcc75abf4e4
Trey Wood <treyw@squareup.com>
2022-10-12 13:48:35 -0600

    this is problematic now

--- .config/zsh/config.zsh
+++ .config/zsh/config.zsh
@@ -6,7 +6,6 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
 fi
 
 source "$(brew --prefix)/share/antigen/antigen.zsh"
-antigen use oh-my-zsh
 
 antigen bundle git
 antigen bundle zsh-users/zsh-history-substring-search
