6fc37a899cd55c8e9ca1aee1bf17d0c1045abe84
Trey Wood <treyw@squareup.com>
2022-11-03 14:39:20 -0600

    tinkerings

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -16,6 +16,8 @@ antigen bundle trystan2k/zsh-tab-title
 
 antigen bundle zsh-users/zsh-autosuggestions
 antigen bundle zsh-users/zsh-syntax-highlighting
+export HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='fg=green,bold'
+export HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='fg=red,bold'
 antigen bundle zsh-users/zsh-history-substring-search
 
 export ZVM_LINE_INIT_MODE=$ZVM_MODE_INSERT
@@ -23,6 +25,7 @@ export ZVM_VI_SURROUND_BINDKEY='s-prefix'
 export ZVM_CURSOR_STYLE_ENABLED=false
 export ZVM_VI_HIGHLIGHT_BACKGROUND='green'
 export ZVM_VI_HIGHLIGHT_FOREGROUND='black'
+export ZVM_READKEY_ENGINE=$ZVM_READKEY_ENGINE_ZLE
 antigen bundle jeffreytse/zsh-vi-mode
 
 antigen theme romkatv/powerlevel10k
@@ -31,6 +34,11 @@ antigen apply
 
 POWERLEVEL9k_DISABLE_CONFIGURATION_WIZARD=true
 source ~/.config/zsh/p10k.zsh
+
+bindkey -r '^P'
+bindkey -r '^N'
+bindkey '^P' history-substring-search-up
+bindkey '^N' history-substring-search-down
 
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
