681b185cfdf6db2b0000bba861c7dda0dc131bcf
Trey Wood <treyw@squareup.com>
2022-12-21 15:08:44 -0700

    fixup

---
+++ nvim/lua/options.lua
@@ -0,0 +1,31 @@
+vim.cmd([[
+  filetype plugin indent on
+  set mouse=
+]])
+
+vim.o.backspace = 'indent,eol,start'
+
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+
+vim.o.ruler = true
+vim.o.number = true
+vim.o.showcmd = true
+vim.o.incsearch = true
+vim.o.hlsearch = true
+vim.o.path = os.getenv('PWD') .. '/**'
+vim.o.encoding = 'UTF-8'
+vim.o.completeopt = 'menu,menuone,noselect'
+vim.o.termguicolors = true
+vim.o.updatetime = 100
+vim.o.cursorline = false
+vim.o.splitright = true
+
+vim.wo.foldmethod = 'expr'
+vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.o.foldlevelstart = 99
+
+vim.o.lazyredraw = true
+
+vim.g.mapleader = ','
