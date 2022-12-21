5a64d48838fed74b23c564dc1b0247b9d5814e42
Trey Wood <treyw@squareup.com>
2022-12-21 14:49:25 -0700

    lazy.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,37 +1,39 @@
-require('plugins')
-require('options')
-require('theme')
-require('keymaps').setup()
-require('lsp')
-require('autocommands')
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--single-branch",
+		"https://github.com/folke/lazy.nvim.git",
+		lazypath,
+	})
+end
+vim.opt.runtimepath:prepend(lazypath)
 
-vim.api.nvim_create_user_command('PC', 'PackerCompile', {})
-vim.api.nvim_create_user_command('PS', 'PackerSync', {})
+require("options")
+require("lazy").setup("plugins")
+require("theme")
+require("keymaps").setup()
+require("lsp")
+require("autocommands")
 
-vim.api.nvim_create_user_command('Notes', function(opts)
-  local name = opts.fargs[1] or 'notes'
-  vim.cmd(string.format('tabe ~/notes/%s', name))
+vim.api.nvim_create_user_command("PC", "PackerCompile", {})
+vim.api.nvim_create_user_command("PS", "PackerSync", {})
+
+vim.api.nvim_create_user_command("Kochiku", function(opts)
+	local command = "!sq kochiku"
+	if #opts.fargs > 0 then
+		command = string.format("%s %s", command, table.concat(opts.fargs, " "))
+	end
+	vim.cmd(command)
 end, {
-  nargs = '?',
-  force = true,
-  complete = function()
-    return vim.fn.readdir(string.format('%s/notes/', os.getenv('HOME')))
-  end,
+	nargs = "?",
+	force = true,
+	complete = function()
+		return { "--canary" }
+	end,
 })
-
-vim.api.nvim_create_user_command('Kochiku', function(opts)
-  local command = '!sq kochiku'
-  if #opts.fargs > 0 then
-    command = string.format('%s %s', command, table.concat(opts.fargs, ' '))
-  end
-  vim.cmd(command)
-end, {
-  nargs = '?',
-  force = true,
-  complete = function()
-    return { '--canary' }
-  end,
-})
-vim.api.nvim_create_user_command('Migrate', function()
-  vim.cmd('!rails db:migrate && rails db:migrate RAILS_ENV=test')
+vim.api.nvim_create_user_command("Migrate", function()
+	vim.cmd("!rails db:migrate && rails db:migrate RAILS_ENV=test")
 end, {})
