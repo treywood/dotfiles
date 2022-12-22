8d4034f904eaa442ac4250f685ba91c6980dabf5
Trey Wood <treyw@squareup.com>
2022-12-21 22:39:31 -0700

    telescope plugins

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,59 @@
+local M = {
+  'nvim-telescope/telescope.nvim',
+  dependencies = {
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    'nvim-telescope/telescope-live-grep-args.nvim',
+  },
+}
+
+function M.config()
+  local telescope = require('telescope')
+  local actions = require('telescope.actions')
+  local lga_actions = require('telescope-live-grep-args.actions')
+
+  telescope.setup {
+    defaults = {
+      layout_strategy = 'bottom_pane',
+      layout_config = {
+        height = 0.4,
+        prompt_position = 'bottom',
+      },
+      selection_caret = '  ',
+      mappings = {
+        i = {
+          ['<C-n>'] = false,
+          ['<C-p>'] = false,
+          ['<C-j>'] = actions.move_selection_next,
+          ['<C-k>'] = actions.move_selection_previous,
+          ['<C-c>'] = false,
+          ['<Esc>'] = actions.close,
+        },
+      },
+    },
+    pickers = {
+      buffers = {
+        mappings = {
+          i = {
+            ['<C-w>'] = actions.delete_buffer,
+          },
+        },
+      },
+    },
+    extensions = {
+      'fzf',
+      live_grep_args = {
+        mappings = {
+          i = {
+            ['<C-k>'] = actions.move_selection_previous,
+            ['<C-l>'] = lga_actions.quote_prompt(),
+          },
+        },
+      },
+    },
+  }
+
+  telescope.load_extension('fzf')
+  telescope.load_extension('live_grep_args')
+end
+
+return M
