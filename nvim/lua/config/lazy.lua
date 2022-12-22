5f556a93d9efbb1cf220c8f0dc7879f06e4ec277
Trey Wood <treyw@squareup.com>
2022-12-22 08:58:14 -0700

    local lazy config

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,5 +1,12 @@
-require('lazy').setup('config.plugins', {
+local lazy_config = {
   checker = {
     enabled = true,
   },
-})
+}
+
+local ok, local_config = pcall(require, 'config.lazy.config')
+if ok then
+  lazy_config = vim.tbl_deep_extend('force', local_config)
+end
+
+require('lazy').setup('config.plugins', lazy_config)
