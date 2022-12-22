722f77140d1edf5781d989bb0eb9ccb9ed91ad35
Trey Wood <treyw@squareup.com>
2022-12-21 22:25:34 -0700

    lsp plugins

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -56,7 +56,7 @@ return {
 
       telescope.load_extension('fzf')
       telescope.load_extension('live_grep_args')
-    end
+    end,
   },
   { 'nvim-tree/nvim-web-devicons', lazy = true },
   {
@@ -90,91 +90,6 @@ return {
     end,
   },
   'hrsh7th/cmp-nvim-lsp',
-  {
-    'neovim/nvim-lspconfig',
-    config = function ()
-      -- Use a loop to conveniently call 'setup' on multiple servers and
-      -- map buffer local keybindings when the language server attaches
-      local have_servers, servers = pcall(require, 'lsp_servers')
-      if have_servers then
-        local nvim_lsp = require('lspconfig')
-
-        -- Setup lspconfig.
-        local capabilities = require('cmp_nvim_lsp').default_capabilities()
-        capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-        -- Use an on_attach function to only map the following keys
-        -- after the language server attaches to the current buffer
-        local on_attach = function(client, bufnr)
-          require('keymaps').setup_lsp(bufnr)
-          require('illuminate').on_attach(client)
-          client.server_capabilities.documentFormattingProvider = false
-          client.server_capabilities.documentRangeFormattingProvider = false
-        end
-
-        for _, lsp in ipairs(servers) do
-          local lsp_name = lsp
-          local config = {
-            on_attach = on_attach,
-            capabilities = capabilities,
-            flags = {
-              debounce_text_changes = 150,
-            },
-          }
-
-          if type(lsp) == 'table' then
-            lsp_name = table.remove(lsp, 1)
-            config = vim.tbl_deep_extend('force', config, lsp)
-          end
-
-          nvim_lsp[lsp_name].setup(config)
-        end
-      end
-    end
-  },
-  {
-    'jose-elias-alvarez/null-ls.nvim',
-    config = function ()
-      local have_sources, sources = pcall(require, 'null_ls_sources')
-      if have_sources then
-        require('null-ls').setup {
-          debug = true,
-          sources = sources,
-          on_attach = function(client, bufnr)
-            if client.server_capabilities.documentFormattingProvider then
-              vim.api.nvim_create_autocmd('BufWritePre', {
-                buffer = bufnr,
-                callback = function()
-                  vim.lsp.buf.format { async = false }
-                end,
-              })
-            end
-          end,
-        }
-      end
-    end
-  },
-  {
-    'j-hui/fidget.nvim',
-    config = function()
-      require('fidget').setup {
-        text = {
-          spinner = 'dots_snake',
-        },
-      }
-    end
-  },
-  {
-    'RRethy/vim-illuminate',
-    init = function()
-      vim.g.Illuminate_delay = 500
-    end,
-    config = function()
-      require('illuminate').configure {
-        filetypes_denylist = { 'NvimTree' },
-      }
-    end,
-  }, 
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,77 @@
+local M = {
+  'williamboman/mason.nvim',
+  dependencies = {
+    'williamboman/mason-lspconfig.nvim',
+    'jayp0521/mason-null-ls.nvim',
+    'neovim/nvim-lspconfig',
+    'jose-elias-alvarez/null-ls.nvim',
+    'j-hui/fidget.nvim',
+    'RRethy/vim-illuminate',
+  },
+}
+
+function M.init()
+  vim.g.Illuminate_delay = 500
+end
+
+function M.config()
+  require('mason').setup {
+    ensure_installed = { 'sumneko_lua' },
+  }
+  require('mason-lspconfig').setup()
+
+  local lsp_config = require('lspconfig')
+  require('mason-lspconfig').setup_handlers {
+    function(server_name)
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+      local on_attach = function(client, bufnr)
+        require('keymaps').setup_lsp(bufnr)
+        require('illuminate').on_attach(client)
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
+      end
+
+      local config = {
+        on_attach = on_attach,
+        capabilities = capabilities,
+        flags = {
+          debounce_text_changes = 150,
+        },
+      }
+      lsp_config[server_name].setup(config)
+    end,
+  }
+
+  require('mason-null-ls').setup {
+    ensure_installed = { 'stylua' },
+    automatic_setup = true,
+  }
+  require('null-ls').setup {
+    debug = true,
+    on_attach = function(client, bufnr)
+      if client.server_capabilities.documentFormattingProvider then
+        vim.api.nvim_create_autocmd('BufWritePre', {
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.format { async = false }
+          end,
+        })
+      end
+    end,
+  }
+  require('mason-null-ls').setup_handlers()
+
+  require('fidget').setup {
+    text = {
+      spinner = 'dots_snake',
+    },
+  }
+
+  require('illuminate').configure {
+    filetypes_denylist = { 'NvimTree' },
+  }
+end
+
+return M
