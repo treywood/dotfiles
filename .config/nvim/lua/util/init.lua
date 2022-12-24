1b7996cee887486281d7c973a8118c749b8ee7e9
Trey Wood <treyw@squareup.com>
2022-12-24 13:11:29 -0700

    lsp stuff

---
+++ .config/nvim/lua/util/init.lua
@@ -0,0 +1,27 @@
+local M = {}
+
+function M.ls(path, fn)
+  local handle = vim.loop.fs_scandir(path)
+  while handle do
+    local name = vim.loop.fs_scandir_next(handle)
+    if not name then
+      break
+    end
+
+    if fn(name) == false then
+      break
+    end
+  end
+end
+
+local lua_root = vim.fn.stdpath('config') .. '/lua/'
+function M.lsmod(modpath, fn)
+  local path = lua_root .. modpath:gsub('%.', '/')
+  return M.ls(path, function(name)
+    if name:sub(-4) == '.lua' then
+      fn(modpath .. '.' .. name:sub(1, -5))
+    end
+  end)
+end
+
+return M
