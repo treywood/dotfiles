cf3d1274b1ade168e0b0b94ecfbbaa2f1e44819e
Trey Wood <treyw@squareup.com>
2022-12-25 23:19:16 -0700

    lsp utils refactor

--- .config/nvim/lua/config/lsp/init.lua
+++ .config/nvim/lua/config/lsp/init.lua
@@ -2,13 +2,7 @@ local util = require('config.lsp.util')
 
 require('mason-lspconfig').setup_handlers {
   function(server_name)
-    require('lspconfig')[server_name].setup {
-      on_attach = util.on_attach,
-      capabilities = util.get_capabilities(),
-      flags = {
-        debounce_text_changes = 150,
-      },
-    }
+    util.setup(server_name)
   end,
 }
 
--- .config/nvim/lua/config/lsp/util.lua
+++ .config/nvim/lua/config/lsp/util.lua
@@ -13,4 +13,19 @@ function M.on_attach(client, bufnr)
   client.server_capabilities.documentRangeFormattingProvider = false
 end
 
+function M.setup(server_name)
+  require('lspconfig')[server_name].setup {
+    on_attach = M.on_attach,
+    capabilities = M.get_capabilities(),
+    flags = {
+      debounce_text_changes = 150,
+    },
+  }
+end
+
+function M.configure(server_name, config)
+  require('lspconfig.configs')[server_name] = config
+  M.setup(server_name)
+end
+
 return M
