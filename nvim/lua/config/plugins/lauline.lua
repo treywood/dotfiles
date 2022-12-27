aad08385170727acc642331587caf907c722484d
Trey Wood <treyw@squareup.com>
2022-12-27 10:40:13 -0700

    more refactor

---
+++ nvim/lua/config/plugins/lauline.lua
@@ -0,0 +1,34 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  config = {
+    options = {
+      theme = 'everforest',
+      disabled_filetypes = {
+        statusline = { 'NvimTree' },
+      },
+    },
+    sections = {
+      lualine_b = { { 'branch', icon = '' } },
+      lualine_c = { { 'filename', file_status = true, path = 1 } },
+      lualine_x = {
+        {
+          require('lazy.status').updates,
+          cond = require('lazy.status').has_updates,
+        },
+      },
+      lualine_y = { 'filetype' },
+    },
+    tabline = {
+      lualine_a = { { 'tabs', mode = 1 } },
+      lualine_z = {},
+    },
+    extensions = {
+      {
+        filetypes = { 'fugitive' },
+        sections = {
+          lualine_a = { { 'branch', icon = '' } },
+        },
+      },
+    },
+  },
+}
