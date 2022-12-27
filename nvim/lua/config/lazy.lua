7898d0366d100e2440e7b977cec427e12fb1cd2d
Trey Wood <treyw@squareup.com>
2022-12-27 09:13:41 -0700

    lazy icons

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -2,6 +2,21 @@ local lazy_config = {
   checker = {
     enabled = true,
   },
+  ui = {
+    icons = {
+      cmd = '⌘',
+      config = '',
+      event = '',
+      ft = '',
+      init = '⚙',
+      keys = '',
+      plugin = '',
+      runtime = '',
+      source = '',
+      start = '',
+      task = '',
+    },
+  },
 }
 
 local ok, local_config = pcall(require, 'config.lazy.config')
