0339365664724b1c638118d26e92ee51b805a6cc
Trey Wood <treyw@squareup.com>
2022-12-28 09:18:50 -0700

    refactor

--- nvim/lua/config/plugins/lsp.lua
+++ nvim/lua/config/plugins/lsp.lua
@@ -1,49 +1,56 @@
 local util = require('util')
 
-local M = {
-  'neovim/nvim-lspconfig',
-  event = 'VeryLazy',
-  dependencies = {
+return {
+  {
+    'neovim/nvim-lspconfig',
+    event = 'VeryLazy',
+    init = function()
+      vim.diagnostic.config {
+        virtual_text = false,
+      }
+    end,
+    config = function()
+      util.lsmod('config.lsp', function(mod)
+        pcall(require, mod)
+      end)
+    end,
+  },
+  {
     'jose-elias-alvarez/null-ls.nvim',
+    event = 'VeryLazy',
+    config = {
+      debug = true,
+      on_attach = function(client, bufnr)
+        if client.server_capabilities.documentFormattingProvider then
+          vim.api.nvim_create_autocmd('BufWritePre', {
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format { async = false }
+            end,
+          })
+        end
+      end,
+    },
+  },
+  {
     'j-hui/fidget.nvim',
+    event = 'VeryLazy',
+    config = {
+      text = {
+        spinner = 'dots_snake',
+      },
+    },
+  },
+  {
     'RRethy/vim-illuminate',
+    event = 'VeryLazy',
+    init = function()
+      vim.g.Illuminate_delay = 500
+    end,
+    config = function()
+      require('illuminate').configure {
+        filetypes_denylist = { 'NvimTree' },
+      }
+    end,
   },
 }
-
-function M.init()
-  vim.g.Illuminate_delay = 500
-  vim.diagnostic.config {
-    virtual_text = false,
-  }
-end
-
-function M.config()
-  util.lsmod('config.lsp', function(mod)
-    pcall(require, mod)
-  end)
-
-  require('null-ls').setup {
-    debug = true,
-    on_attach = function(client, bufnr)
-      if client.server_capabilities.documentFormattingProvider then
-        vim.api.nvim_create_autocmd('BufWritePre', {
-          buffer = bufnr,
-          callback = function()
-            vim.lsp.buf.format { async = false }
-          end,
-        })
-      end
-    end,
-  }
-
-  require('fidget').setup {
-    text = {
-      spinner = 'dots_snake',
-    },
-  }
-  require('illuminate').configure {
-    filetypes_denylist = { 'NvimTree' },
-  }
-end
-
-return M
