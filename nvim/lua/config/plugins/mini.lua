60cecada62f7d430979dbb1dbed75dd61e7abdcb
Trey Wood <treyw@squareup.com>
2022-12-29 08:52:50 -0700

    also mini

--- nvim/lua/config/plugins/mini.lua
+++ nvim/lua/config/plugins/mini.lua
@@ -1,31 +1,29 @@
-local M = {
+return {
   'echasnovski/mini.nvim',
+  branch = 'starter-ensure-normal',
+  config = function()
+    require('mini.comment').setup {
+      mappings = {
+        comment = 'm',
+        comment_line = 'mm',
+        textobject = 'm',
+      },
+    }
+
+    local starter = require('mini.starter')
+    starter.setup {
+      header = vim.fn.system('fortune'),
+      items = {
+        starter.sections.recent_files(10, true),
+      },
+      content_hooks = {
+        starter.gen_hook.adding_bullet('· '),
+        starter.gen_hook.padding(15),
+        starter.gen_hook.aligning('left', 'center'),
+      },
+    }
+
+    require('mini.pairs').setup()
+    require('mini.surround').setup()
+  end,
 }
-
-function M.config()
-  require('mini.comment').setup {
-    mappings = {
-      comment = 'm',
-      comment_line = 'mm',
-      textobject = 'm',
-    },
-  }
-
-  local starter = require('mini.starter')
-  starter.setup {
-    header = vim.fn.system('fortune'),
-    items = {
-      starter.sections.recent_files(10, true),
-    },
-    content_hooks = {
-      starter.gen_hook.adding_bullet('· '),
-      starter.gen_hook.padding(15),
-      starter.gen_hook.aligning('left', 'center'),
-    },
-  }
-
-  require('mini.pairs').setup()
-  require('mini.surround').setup()
-end
-
-return M
