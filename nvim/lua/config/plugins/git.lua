323b9a3529b97017e9511c9823855ffabb21ab88
Trey Wood <treyw@squareup.com>
2023-01-01 23:05:15 -0700

    lazy git plugins

--- nvim/lua/config/plugins/git.lua
+++ nvim/lua/config/plugins/git.lua
@@ -1,7 +1,18 @@
 return {
-  'tpope/vim-fugitive',
+  {
+    'tpope/vim-rhubarb',
+    dependencies = {
+      'tpope/vim-fugitive',
+      cmd = { 'G', 'Git' },
+    },
+    keys = {
+      { '<leader>gy', ':GBrowse<cr>' },
+      { '<leader>gy', ':GBrowse!<cr>', mode = 'v' },
+    },
+  },
   {
     'lewis6991/gitsigns.nvim',
+    event = { 'BufNewFile', 'BufReadPre' },
     config = function()
       require('gitsigns').setup()
       vim.keymap.set('n', ']c', '<cmd>Gitsigns next_hunk<cr>')
