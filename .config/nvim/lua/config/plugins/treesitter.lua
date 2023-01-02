03a084f9789e301a47822dbf53e0f2592b99ea54
Trey Wood <treyw@squareup.com>
2023-01-01 23:10:09 -0700

    lazy lazy

--- .config/nvim/lua/config/plugins/treesitter.lua
+++ .config/nvim/lua/config/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
-    event = 'VeryLazy',
+    event = { 'BufNewFile', 'BufReadPre' },
     build = ':TSUpdate',
     dependencies = {
       'RRethy/nvim-treesitter-endwise',
