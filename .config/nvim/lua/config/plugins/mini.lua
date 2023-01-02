f78a60bebaae8d8f38ad7be30a769ad0745700e0
Trey Wood <treyw@squareup.com>
2023-01-02 00:42:28 -0700

    lazy lazy lazy

--- .config/nvim/lua/config/plugins/mini.lua
+++ .config/nvim/lua/config/plugins/mini.lua
@@ -1,28 +1,52 @@
 return {
-  'echasnovski/mini.nvim',
-  config = function()
-    require('mini.comment').setup {
-      mappings = {
-        comment = 'm',
-        comment_line = 'mm',
-        textobject = 'm',
-      },
-    }
-
-    local starter = require('mini.starter')
-    starter.setup {
-      header = vim.fn.system('fortune'),
-      items = {
-        starter.sections.recent_files(10, true),
-      },
-      content_hooks = {
-        starter.gen_hook.adding_bullet('· '),
-        starter.gen_hook.padding(15),
-        starter.gen_hook.aligning('left', 'center'),
-      },
-    }
-
-    require('mini.pairs').setup()
-    require('mini.surround').setup()
-  end,
+  {
+    'echasnovski/mini.starter',
+    config = function()
+      local starter = require('mini.starter')
+      starter.setup {
+        header = vim.fn.system('fortune'),
+        items = {
+          starter.sections.recent_files(10, true),
+        },
+        content_hooks = {
+          starter.gen_hook.adding_bullet('· '),
+          starter.gen_hook.padding(15),
+          starter.gen_hook.aligning('left', 'center'),
+        },
+      }
+    end,
+  },
+  {
+    'echasnovski/mini.pairs',
+    name = 'mini.pairs',
+    branch = 'stable',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+      require('mini.pairs').setup()
+    end,
+  },
+  {
+    'echasnovski/mini.surround',
+    name = 'mini.surround',
+    branch = 'stable',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+      require('mini.surround').setup()
+    end,
+  },
+  {
+    'echasnovski/mini.comment',
+    name = 'mini.comment',
+    branch = 'stable',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+      require('mini.comment').setup {
+        mappings = {
+          comment = 'm',
+          comment_line = 'mm',
+          textobject = 'm',
+        },
+      }
+    end,
+  },
 }
