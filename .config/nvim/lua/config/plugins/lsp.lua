b60e3ce6cad0a8b88f130ef93b85f45df1e68d09
Trey Wood <treyw@squareup.com>
2023-01-03 22:23:31 -0700

    reorganize lsp plugins

--- .config/nvim/lua/config/plugins/lsp.lua
+++ .config/nvim/lua/config/plugins/lsp.lua
@@ -4,7 +4,34 @@ return {
   {
     'neovim/nvim-lspconfig',
     event = { 'BufNewFile', 'BufReadPre' },
+    dependencies = {
+      {
+        'jose-elias-alvarez/null-ls.nvim',
+        config = {
+          debug = true,
+          on_attach = function(client, bufnr)
+            if client.server_capabilities.documentFormattingProvider then
+              vim.api.nvim_create_autocmd('BufWritePre', {
+                buffer = bufnr,
+                callback = function()
+                  vim.lsp.buf.format { async = false }
+                end,
+              })
+            end
+          end,
+        },
+      },
+      {
+        'RRethy/vim-illuminate',
+        config = function()
+          require('illuminate').configure {
+            filetypes_denylist = { 'NvimTree' },
+          }
+        end,
+      },
+    },
     init = function()
+      vim.g.Illuminate_delay = 500
       vim.diagnostic.config {
         virtual_text = false,
       }
@@ -16,37 +43,8 @@ return {
     end,
   },
   {
-    'jose-elias-alvarez/null-ls.nvim',
-    lazy = true,
-    config = {
-      debug = true,
-      on_attach = function(client, bufnr)
-        if client.server_capabilities.documentFormattingProvider then
-          vim.api.nvim_create_autocmd('BufWritePre', {
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format { async = false }
-            end,
-          })
-        end
-      end,
-    },
-  },
-  {
     'smjonas/inc-rename.nvim',
     cmd = 'IncRename',
     config = true,
-  },
-  {
-    'RRethy/vim-illuminate',
-    lazy = true,
-    init = function()
-      vim.g.Illuminate_delay = 500
-    end,
-    config = function()
-      require('illuminate').configure {
-        filetypes_denylist = { 'NvimTree' },
-      }
-    end,
   },
 }
