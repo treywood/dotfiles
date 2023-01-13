aff360c6b4b419a2ff44ecff4db1931f88a1d5b2
Trey Wood <treyw@squareup.com>
2023-01-13 09:22:41 -0700

    refactor hidden

--- .config/nvim/lua/config/plugins/hidden.lua
+++ .config/nvim/lua/config/plugins/hidden.lua
@@ -1,18 +1,1 @@
-local specs = {}
-local root = vim.fn.stdpath('config') .. '/lua/config/plugins/hidden/'
-
-if vim.loop.fs_stat(root) then
-  local util = require('util')
-  util.lsmod('config.plugins.hidden', function(module)
-    local ok, spec = pcall(require, module)
-    if ok then
-      table.insert(specs, spec)
-      return true
-    else
-      print('error loading ' .. module)
-      return false
-    end
-  end)
-end
-
-return specs
+return {}
