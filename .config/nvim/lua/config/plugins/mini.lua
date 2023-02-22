ab4d80505f64bc5ffe919f936356272f89295938
Trey Wood <treyw@squareup.com>
2023-02-22 10:27:48 -0700

    mini things

--- .config/nvim/lua/config/plugins/mini.lua
+++ .config/nvim/lua/config/plugins/mini.lua
@@ -1,32 +1,57 @@
 return {
-  'echasnovski/mini.nvim',
-  config = function()
-    local starter = require('mini.starter')
-    starter.setup {
-      header = vim.fn.system('fortune'),
-      items = {
-        starter.sections.recent_files(10, true),
-      },
-      content_hooks = {
-        starter.gen_hook.adding_bullet('· '),
-        starter.gen_hook.padding(15),
-        starter.gen_hook.aligning('left', 'center'),
-      },
-    }
-
-    require('mini.comment').setup {
+  {
+    'echasnovski/mini.starter',
+    config = function()
+      local starter = require('mini.starter')
+      starter.setup {
+        header = vim.fn.system('fortune'),
+        items = {
+          starter.sections.recent_files(10, true),
+        },
+        content_hooks = {
+          starter.gen_hook.adding_bullet('· '),
+          starter.gen_hook.padding(15),
+          starter.gen_hook.aligning('left', 'center'),
+        },
+      }
+    end,
+  },
+  {
+    'echasnovski/mini.comment',
+    event = { 'BufNewFile', 'BufReadPre' },
+    opts = {
       mappings = {
         comment = 'm',
         comment_line = 'mm',
         textobject = 'm',
       },
-    }
-
-    require('mini.bracketed').setup {
+    },
+    config = function(_, opts)
+      require('mini.comment').setup(opts)
+    end,
+  },
+  {
+    'echasnovski/mini.bracketed',
+    event = { 'BufNewFile', 'BufReadPre' },
+    opts = {
       comment = { suffix = 'm' },
-    }
-
-    require('mini.pairs').setup()
-    require('mini.surround').setup()
-  end,
+    },
+    config = function(_, opts)
+      require('mini.bracketed').setup(opts)
+    end,
+  },
+  {
+    'echasnovski/mini.pairs',
+    event = { 'BufNewFile', 'BufReadPre' },
+    config = function()
+      require('mini.pairs').setup()
+    end,
+  },
+  {
+    'echasnovski/mini.surround',
+    event = { 'BufNewFile', 'BufReadPre' },
+    config = function()
+      require('mini.surround').setup()
+    end,
+  },
 }
