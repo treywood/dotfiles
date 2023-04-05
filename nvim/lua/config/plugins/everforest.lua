c6eaa22897cf779c979634fd259061a054aa236f
Trey Wood <treyw@squareup.com>
2023-04-05 11:05:52 -0600

    clean up highlights

--- nvim/lua/config/plugins/everforest.lua
+++ nvim/lua/config/plugins/everforest.lua
@@ -1,3 +1,9 @@
+local function hl_links(table)
+  for hl_group, target in pairs(table) do
+    vim.api.nvim_set_hl(0, hl_group, { link = target })
+  end
+end
+
 return {
   'sainnhe/everforest',
   priority = 1000,
@@ -13,45 +19,53 @@ return {
     vim.api.nvim_set_hl(0, 'Gray', { fg = '#a7b0a4' })
     vim.api.nvim_set_hl(0, 'GreenBold', { fg = '#a7c080', bold = true })
 
-    vim.api.nvim_set_hl(0, '@parameter', { link = 'Gray' })
-    vim.api.nvim_set_hl(0, '@operator', { link = 'Gray' })
-    vim.api.nvim_set_hl(0, '@field', { link = '@variable' })
-    vim.api.nvim_set_hl(0, '@property', { link = '@field' })
-    vim.api.nvim_set_hl(0, '@enumMember', { link = '@field' })
+    hl_links {
+      ['@parameter'] = 'Gray',
+      ['@operator'] = 'Gray',
+      ['@field'] = '@variable',
+      ['@property'] = '@field',
+      ['@lsp.type.property'] = '@property',
+      ['@enumMember'] = '@field',
+      ['@macro'] = '@function',
+      ['@lsp.type.namespace'] = '@type',
+      ['@lsp.type.parameter'] = '@identifier',
+      ['@lsp.typemod.parameter.declaration'] = '@parameter',
+      ['@identifier'] = 'Fg',
 
-    vim.api.nvim_set_hl(0, '@symbol.ruby', { link = 'Orange' })
-    vim.api.nvim_set_hl(0, '@label.ruby', { link = '@variable' })
+      -- ruby
+      ['@symbol.ruby'] = 'Orange',
+      ['@label.ruby'] = '@variable',
 
-    vim.api.nvim_set_hl(0, '@macro', { link = '@function' })
+      -- go
+      goTSProperty = '@field',
+      goTSConstBuiltin = '@constant.builtin',
 
-    vim.api.nvim_set_hl(0, 'goTSProperty', { link = '@field' })
-    vim.api.nvim_set_hl(0, 'goTSConstBuiltin', { link = '@constant.builtin' })
+      -- yaml
+      ['@field.yaml'] = 'Orange',
+      yamlBlockMappingKey = 'Orange',
+      yamlPlainScalar = 'String',
+      yamlFlowString = 'String',
+      yamlFlowStringDelimiter = 'String',
+      yamlBlockCollectionItemStart = 'Gray',
 
-    vim.api.nvim_set_hl(0, 'Identifier', { link = 'Fg' })
+      -- json
+      ['@label.json'] = 'Orange',
+      ['@string.json'] = '@string',
 
-    vim.api.nvim_set_hl(0, '@field.yaml', { link = 'Orange' })
-    vim.api.nvim_set_hl(0, 'yamlBlockMappingKey', { link = 'Orange' })
-    vim.api.nvim_set_hl(0, 'yamlPlainScalar', { link = 'String' })
-    vim.api.nvim_set_hl(0, 'yamlFlowString', { link = 'String' })
-    vim.api.nvim_set_hl(0, 'yamlFlowStringDelimiter', { link = 'String' })
-    vim.api.nvim_set_hl(0, 'yamlBlockCollectionItemStart', { link = 'Gray' })
+      GitSignsAdd = 'GreenSign',
+      GitSignsChange = 'OrangeSign',
+      GitSignsDelete = 'RedSign',
 
-    vim.api.nvim_set_hl(0, '@label.json', { link = 'Orange' })
-    vim.api.nvim_set_hl(0, '@string.json', { link = '@string' })
+      MiniStarterHeader = 'Green',
+      MiniStarterSection = 'Red',
+      MiniStarterItem = 'Gray',
+      MiniStarterItemBullet = 'GreenBold',
+      MiniStarterItemPrefix = 'GreenBold',
+      MiniStarterQuery = 'Orange',
+      MiniStarterFooter = 'Gray',
 
-    vim.api.nvim_set_hl(0, 'GitSignsAdd', { link = 'GreenSign' })
-    vim.api.nvim_set_hl(0, 'GitSignsChange', { link = 'OrangeSign' })
-    vim.api.nvim_set_hl(0, 'GitSignsDelete', { link = 'RedSign' })
-
-    vim.api.nvim_set_hl(0, 'MiniStarterHeader', { link = 'Green' })
-    vim.api.nvim_set_hl(0, 'MiniStarterSection', { link = 'Red' })
-    vim.api.nvim_set_hl(0, 'MiniStarterItem', { link = 'Gray' })
-    vim.api.nvim_set_hl(0, 'MiniStarterItemBullet', { link = 'GreenBold' })
-    vim.api.nvim_set_hl(0, 'MiniStarterItemPrefix', { link = 'GreenBold' })
-    vim.api.nvim_set_hl(0, 'MiniStarterQuery', { link = 'Orange' })
-    vim.api.nvim_set_hl(0, 'MiniStarterFooter', { link = 'Gray' })
-
-    vim.api.nvim_set_hl(0, 'DevIconRb', { link = 'Red' })
+      DevIconRb = 'Red',
+    }
 
     vim.cmd([[
       hi ErrorText gui=none |
