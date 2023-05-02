e468af15b6793fded5d0241a727853db496fb932
Trey Wood <treyw@squareup.com>
2023-05-01 22:50:30 -0600

    LspRestart

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -84,3 +84,20 @@ vim.api.nvim_create_autocmd({ 'BufReadPre', 'BufNewFile' }, {
     end
   end,
 })
+
+vim.api.nvim_create_user_command('LspRestart', function()
+  local lsp_clients = vim.lsp.buf_get_clients()
+  for _, client in pairs(lsp_clients) do
+    local config = client.config
+    local function restart()
+      if client.is_stopped() then
+        vim.lsp.start(config)
+      else
+        vim.defer_fn(restart, 500)
+      end
+    end
+
+    client.stop()
+    vim.defer_fn(restart, 500)
+  end
+end, {})
