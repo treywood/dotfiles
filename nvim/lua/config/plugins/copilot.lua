4db6315e44464e650a1d947014d4fc731880140c
Trey Wood <treyw@squareup.com>
2023-10-27 23:10:24 -0600

    fix copilot config for not-m1-macs

--- nvim/lua/config/plugins/copilot.lua
+++ nvim/lua/config/plugins/copilot.lua
@@ -1,9 +1,11 @@
+local brew_prefix = vim.fn.system('brew --prefix'):gsub('%s+', '')
+
 return {
   'zbirenbaum/copilot.lua',
   cmd = 'Copilot',
   event = 'InsertEnter',
   config = {
-    copilot_node_command = '/opt/homebrew/bin/node',
+    copilot_node_command = brew_prefix .. '/bin/node',
     suggestion = {
       auto_trigger = true,
       keymap = {
