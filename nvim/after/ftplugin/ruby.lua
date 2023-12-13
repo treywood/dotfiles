9b4bc3a8232d9072440d459cea8e64a1c4b12cd4
Trey Wood <treyw@squareup.com>
2023-12-13 11:25:31 -0700

    ruby-lsp

--- nvim/after/ftplugin/ruby.lua
+++ nvim/after/ftplugin/ruby.lua
@@ -2,7 +2,7 @@ local util = require('util')
 local lsp = require('util.lsp')
 
 lsp.setup {
-  name = 'solargraph',
-  cmd = { 'solargraph', 'stdio' },
+  name = 'ruby-lsp',
+  cmd = { 'ruby-lsp' },
   root_dir = util.root_pattern { 'Gemfile' },
 }
