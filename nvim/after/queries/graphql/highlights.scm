19d6f6e0f492562e4c32546de5455929ca3eb7f1
Trey Wood <treyw@squareup.com>
2024-02-16 10:28:35 -0700

    highlights and queries

--- nvim/after/queries/graphql/highlights.scm
+++ nvim/after/queries/graphql/highlights.scm
@@ -1,7 +1,9 @@
 ;; extends
 
 (operation_definition (name) @function (variable_definitions))
+(field (name) @property)
 (field (name) @function.call (arguments))
+(enum_value (name) @constant)
 
 (comma) @punctuation.delimiter
  
