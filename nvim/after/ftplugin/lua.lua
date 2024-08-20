c5c536dc65cea690d3608737f1bde39b3a3a2b57
Trey Wood <treyw@squareup.com>
2024-08-20 10:12:59 -0600

    conditionally format files

--- nvim/after/ftplugin/lua.lua
+++ nvim/after/ftplugin/lua.lua
@@ -18,4 +18,8 @@ lsp.setup {
 format.setup {
   name = 'stylua',
   cmd = { 'stylua', '--search-parent-directories', '--stdin-filepath', '$FILENAME', '-' },
+  test = function()
+    local test = vim.fs.find({ 'stylua.toml' }, { upward = true, path = vim.fn.expand('%:p:h') })
+    return #test > 0
+  end,
 }
