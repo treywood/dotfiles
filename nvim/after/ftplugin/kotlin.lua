3d59d93fe7896832fa28660c06ce76f5cdd32997
Trey Wood <treyw@squareup.com>
2025-05-13 14:48:42 -0600

    intellij things

--- nvim/after/ftplugin/kotlin.lua
+++ nvim/after/ftplugin/kotlin.lua
@@ -4,7 +4,7 @@ local util = require('util')
 lsp.setup {
   name = 'kotlin-language-server',
   cmd = {
-    util.devpath('kotlin-language-server-bazel-support/server/build/install/server/bin/kotlin-language-server'),
+    util.devpath('kotlin-language-server/server/build/install/server/bin/kotlin-language-server'),
   },
-  root_dir = util.root_pattern { 'WORKSPACE' },
+  root_dir = util.root_pattern { 'build.gradle' },
 }
