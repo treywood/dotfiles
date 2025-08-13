7ee9f7ff63a09f9cf5fa94db97d2bc9452d7efdc
Trey Wood <treyw@squareup.com>
2025-08-13 13:49:36 -0600

    kotlin lsp update

--- nvim/after/ftplugin/kotlin.lua
+++ nvim/after/ftplugin/kotlin.lua
@@ -4,6 +4,6 @@ local util = require('util')
 lsp.setup {
   name = 'kotlin-lsp',
   cmd = { 'kotlin-lsp', '--stdio' },
-  root_dir = util.root_pattern { 'BUILD', 'build.gradle.kts' },
+  root_dir = util.root_pattern { 'BUILD', 'build.gradle.kts', 'build.gradle' },
   single_file_support = true,
 }
