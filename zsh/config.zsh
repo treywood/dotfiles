30a8594b74beac36645a10935734a7458d265648
Trey Wood <treyw@squareup.com>
2025-10-27 23:28:07 -0600

    bat everforest

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -9,6 +9,7 @@ fi
 source "$(brew --prefix)/share/antigen/antigen.zsh"
 
 export GIT_COMPLETION_CHECKOUT_NO_GUESS=1
+
 antigen bundle git
 antigen bundle command-not-found
 
@@ -37,27 +38,6 @@ function zvm_after_init() {
 
   # Load fzf keybindings after zsh-vi-mode
   source "$(brew --prefix)/opt/fzf/shell/key-bindings.zsh"
-
-  # Custom fzf history search that uses current buffer as initial query
-  fzf-history-widget-with-query() {
-    local selected
-    setopt localoptions noglobsubst noposixbuiltins pipefail no_aliases 2> /dev/null
-    selected=( $(fc -rl 1 | awk '{ cmd=$0; sub(/^[ \t]*[0-9]+\**[ \t]+/, "", cmd); if (!seen[cmd]++) print $0 }' |
-      FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} ${FZF_DEFAULT_OPTS-} -n2..,.. --scheme=history --bind=ctrl-r:toggle-sort,ctrl-z:ignore ${FZF_CTRL_R_OPTS-} --query=${(qqq)LBUFFER} +m" $(__fzfcmd)) )
-    local ret=$?
-    if [ -n "$selected" ]; then
-      num=$selected[1]
-      if [ -n "$num" ]; then
-        zle vi-fetch-history -n $num
-      fi
-    fi
-    zle reset-prompt
-    return $ret
-  }
-  zle -N fzf-history-widget-with-query
-
-  # Bind Alt-R to fzf history search with current buffer as query
-  bindkey '^[r' fzf-history-widget-with-query
 }
 antigen bundle jeffreytse/zsh-vi-mode
 
