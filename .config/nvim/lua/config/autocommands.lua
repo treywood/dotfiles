b911c1c35c69a10f394e03bb0ea922153845b841
Trey Wood <treyw@squareup.com>
2026-05-28 18:17:01 -0600

    clean up old things

--- .config/nvim/lua/config/autocommands.lua
+++ .config/nvim/lua/config/autocommands.lua
@@ -1,5 +1,3 @@
-local Kitty = require('util.kitty')
-
 vim.api.nvim_create_autocmd('User', {
   pattern = 'MiniStarterOpened',
   callback = function(args)
@@ -17,33 +15,3 @@ vim.api.nvim_create_autocmd('User', {
     vim.keymap.set('n', '<C-p>', ':Telescope find_files<CR>', options)
   end,
 })
-
-if Kitty.is_kitty() then
-  local kitty_window_id = os.getenv('KITTY_WINDOW_ID')
-
-  local function set_tab_title(tab_title)
-    Kitty.remote { 'set-tab-title', '--match', 'window_id:' .. kitty_window_id, tab_title }
-  end
-
-  if vim.fn.argc() == 0 then
-    local async = require('plenary.async')
-    vim.api.nvim_create_autocmd('VimEnter', {
-      callback = async.void(function()
-        local dir_name = string.gsub(vim.fn.getcwd(), '.+/', '')
-        local tab_name = string.format('nvim (%s)', dir_name)
-        set_tab_title(tab_name)
-      end),
-    })
-  end
-  vim.api.nvim_create_autocmd('VimLeave', {
-    callback = function()
-      set_tab_title(nil)
-    end,
-  })
-  vim.api.nvim_create_autocmd('BufWritePost', {
-    pattern = 'kitty.conf',
-    callback = function()
-      Kitty.reload_config()
-    end,
-  })
-end
--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,16 +1,15 @@
 vim.api.nvim_create_user_command('LspRestart', function()
-  local lsp_clients = vim.lsp.buf_get_clients()
-  for _, client in pairs(lsp_clients) do
+  for _, client in ipairs(vim.lsp.get_clients()) do
     local config = client.config
     local function restart()
-      if client.is_stopped() then
+      if client:is_stopped() then
         vim.lsp.start(config)
       else
         vim.defer_fn(restart, 500)
       end
     end
 
-    client.stop()
+    client:stop()
     vim.defer_fn(restart, 500)
   end
 end, {})
